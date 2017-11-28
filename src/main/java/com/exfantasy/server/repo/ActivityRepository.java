5deb3c69b439912d699bb2b75147f5677708da9a
tommyyeh <tommyyeh@192.168.2.142>
2017-11-28 23:30:21 +0800

    新增註解

--- src/main/java/com/exfantasy/server/repo/ActivityRepository.java
+++ src/main/java/com/exfantasy/server/repo/ActivityRepository.java
@@ -7,10 +7,22 @@ import org.springframework.data.mongodb.repository.Query;
 
 import com.exfantasy.server.entity.Activity;
 
+
 public interface ActivityRepository extends MongoRepository<Activity, Long> {
 
 	List<Activity> findByCreateUserId(Long userId);
 
+	/**
+	 * <pre>
+	 * 根據 ActivityId 及 JoinedUserId 查詢
+	 * 
+	 * 參考: <a href="http://javasampleapproach.com/spring-framework/spring-data/mongodb-model-one-one-one-many-relationships-mongodb-embedded-documents-springboot">Spring Boot mongodb one-to-many</a>
+	 * </pre>
+	 * 
+	 * @param activityId
+	 * @param userId
+	 * @return
+	 */
 	@Query("{'id': ?0, 'joinedUsers.id': ?1}")
-	Activity findByActivityIdAndJoinedUserId(Long activityId, Long userId);
+	Activity findByActivityIdAndJoinedUserId(Long activityId, Long joinedUserId);
 }
