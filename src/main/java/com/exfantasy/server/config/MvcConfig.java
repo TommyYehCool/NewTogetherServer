f9ee480db878ee64b4a2a49706921f21b52d9cae
tommyyeh <tommyyeh@192.168.2.142>
2017-11-29 01:25:46 +0800

    頁面調整

--- src/main/java/com/exfantasy/server/config/MvcConfig.java
+++ src/main/java/com/exfantasy/server/config/MvcConfig.java
@@ -25,7 +25,7 @@ public class MvcConfig extends WebMvcConfigurerAdapter {
 	 */
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
-    	registry.addViewController("/").setViewName("home");
+    	registry.addViewController("/").setViewName("index");
     }
     
 }
